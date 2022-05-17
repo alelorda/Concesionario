45fb963ce2f2f4a371a1d1b755b555a6d9b4cd25
Ariel Alexis Lorda Barrabasqui <Ariel.Lorda@inworx.com>
2022-05-17 20:34:33 -0300

    menu user

--- controller/UsuariosControllers.py
+++ controller/UsuariosControllers.py
@@ -17,7 +17,32 @@ def buscarUsuario(correo):
 
 
 def validarContrasenia(password):
-
     if user.password == str(password):
         print('BIENVENIDO')
-        return True
+        return [True, user.nivelUsu]
+
+
+def menu(args):
+    if(args == 1):
+        return menuAdmin()
+    elif args == 2:
+        return menuEmpleado()
+    else:
+        return menuInvitado()
+
+
+def menuAdmin():
+    return "Menu Admin"
+
+
+def menuEmpleado():
+    return "Menu Empleado"
+
+
+def menuInvitado():
+    opcionIngresada = int(input(
+        "Menu Invitado \n 1: Ver Vehiculos \n 2: ver Bicicletas \n 3: Sign Out \nIngrese la operacion deseada:"))
+    if (opcionIngresada == 1):
+        print('Ingreso Opcion 1\n')
+    if(opcionIngresada == 3):
+        return False
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from constructores.Constructores import insertarAuto
 from ui.Views import *
-from controller.UsuariosControllers import buscarUsuario, validarContrasenia
+from controller.UsuariosControllers import buscarUsuario, validarContrasenia, menu
 from constructores.Constructores import insertarAuto
 
 bienvenida()
 userOk = buscarUsuario(msjUsuario())
+intentos = 3
+try:
+    if(userOk):
 
-if(userOk):
+        usuarioconPass = validarContrasenia(msjPassword())
+        while userOk:
+            if(usuarioconPass[0]):
+                userOk = menu(usuarioconPass[1])
+    else:
+        print('No se encontro el Usuario')
 
-    passOk = validarContrasenia(msjPassword())
-    # if(not msjErrorPassword):
-
-else:
-    print('No se encontro el Usuario')
+finally:
+    print('Gracias por Visitarnos')
